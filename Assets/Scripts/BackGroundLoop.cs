222494bef03af353de596a9a1e09988cba12f873
vidya-hub <mavuduru.123.sagar@gmail.com>
2022-06-17 08:38:52 +0530

    Added Cam Logic for first scene loop

--- Assets/Scripts/BackGroundLoop.cs
+++ Assets/Scripts/BackGroundLoop.cs
@@ -26,23 +26,23 @@ public class BackGroundLoop : MonoBehaviour
         {
             // TODO: get the player postion 
             Vector2 playerPosition = player.transform.position;
-
             // TODO: get the width of the background and find the right edge position 
             RectTransform rt = backGround.GetComponent<RectTransform>();
             float width = rt.rect.width;
             float height = rt.rect.height;
             Vector2 backgroundPosition = new Vector2(width, height);
-
             // TODO: find the right edge of the camera
             float cameraEdgePosition = getCameraEdgePoint(mainCamera.transform.position.x);
 
             // TODO: find the distance between right edge and mainCamera Edge position
             float distance = backgroundPosition.x - cameraEdgePosition;
-            Debug.Log(distance);
 
-            // TODO: compare the distance with distance factor 
-            // TODO: if the distance is > df and distance > 0 - display single  
-            // TODO: if the distance is < df or distance <= 0 - copy and place that game object on right side
+            if (distance < distanceFactor && distance <= 0)
+            {
+                // TODO: if the distance is < df or distance <= 0 - copy and place that game object on right side
+                Debug.Log("Crossed");
+            }
+
         }
         float getCameraEdgePoint(float camMidPoint)
         {
